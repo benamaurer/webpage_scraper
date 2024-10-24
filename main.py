eba35e3099b0548e503267b92f48a66ce7c2e770
benamaurer <52429453+benamaurer@users.noreply.github.com>
2024-10-24 13:27:46 -0500

    Update main.py
    Descriptive comments on for loop

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ folders = [
 ]
 
 
-# Verifying required folders exist and creating them if they are missing
-for folder in folders:
-    directory = Path(folder)
-    # Check if the directory exists
-    if not directory.exists():
-        # Create the directory
-        directory.mkdir(parents=True)
-        print(f"Directory '{directory}' seems to be missing, creating it.")
-    else:
-        pass
+# This code block will verify required folders exist and create them if they are missing
+for folder in folders: # Run this code for each folder in our "folders" list we defined above
+    directory = Path(folder) # Create "directory" variable and use our folder string to create a "Path" object
+    if not directory.exists(): # If the directory exists, run the below indented code
+        directory.mkdir(parents=True) # Create the directory (folder) and make sure it has the correct path (that way it is located in the same folder as this python script
+        print(f"Directory '{directory}' seems to be missing, creating it.") # Printing words to the console so we know whats up
+    else: # In the case our "if" clause was not True, run this code
+        pass # Do nothing, since our direcotry already exists
 
 
 # ChatGPT ish below
