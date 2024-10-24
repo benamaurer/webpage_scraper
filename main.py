9f74d27cb36234d99c422899cb0f08f3e3e60a0a
Ben Maurer <ben.a.maurer@gmail.com>
2024-10-24 12:54:45 -0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,15 @@
+# from bs4 import BeautifulSoup
+#
+# # Load the HTML file
+# with open('page.html', 'r', encoding='utf-8') as file:
+#     html_content = file.read()
+#
+# # Parse the HTML with Beautiful Soup
+# soup = BeautifulSoup(html_content, 'html.parser')
+#
+# # Now you can find elements, e.g., all links
+# links = soup.find_all('a')
+# for link in links:
+#     print(link.get('href'))
+
+# ChatGPT ish ^
